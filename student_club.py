5f08df0c2984a9760205548dbb94effd2688dce2
noobshubham <noobshubham65@gmail.com>
2023-07-16 11:25:41 +0530

    u: Increase Limit of Events to 15

--- student_club.py
+++ student_club.py
@@ -12,7 +12,7 @@ def events():
   soup = BeautifulSoup(response.content, "html.parser")
 
   events = []
-  for event in soup.find_all("section", class_="yaqOZd qeLZfd", limit=10):
+  for event in soup.find_all("section", class_="yaqOZd qeLZfd", limit=15):
     title = event.find("p", class_="zfr3Q CDt4Ke").text
     start_date_and_time = event.find_all("p", class_="zfr3Q CDt4Ke")[1].text
     end_date_and_time = event.find_all("p", class_="zfr3Q CDt4Ke")[2].text
